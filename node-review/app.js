0792f138762d99ebb8c1efadb3e6b206fdec18ca
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-07-22 13:52:37 +0100

    completed lv300

--- node-review/app.js
+++ node-review/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 // loading the express module on line one
 console.log("Log 1: ", typeof app);
-
 
 const server = express();
 // initialise a server
 console.log("Log 2: ", typeof server);
 
+const port = 9090;
 
-
-server.get(,);
+server.get("/", function (request, response) {
+  response.send("You've successfully reached the server");
+});
 // set up a "/" endpoint
